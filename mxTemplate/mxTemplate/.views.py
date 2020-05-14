22df1f11d276ad8db578f86cc2a41f626700b395
Nestor Monroy <nestor.monroy.90@gmail.com>
2020-05-14 16:18:06 -0500

    Error
    Error Template

--- mxTemplate/mxTemplate/.views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home_page(request):
-  context = {
-    "title" :"Team"
-  }
-  return render(request, "base.html", context )
