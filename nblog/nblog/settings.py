eff552c6a7f2c5a3be81c9e06cf0f5062f3ca059
NestorMonroy <nestor.monroy.90@gmail.com>
2020-05-22 10:23:47 -0500

    update sass
    add app compresor

--- nblog/nblog/settings.py
+++ nblog/nblog/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
-    'crispy_forms',
+    'compressor',
     'rest_framework',
 
     'accounts',
@@ -120,23 +119,35 @@ USE_L10N = True
 
 USE_TZ = True
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+]
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+    ('text/x-sass', 'django_libsass.SassCompiler'),
+)
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_mxTemplate"),
+]
+
+
 STATIC_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "static_cdn", "static_root")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "static_cdn", "media_root")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_blog"),
-]
 
 PROTECTED_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "static_cdn", "protected_media")
