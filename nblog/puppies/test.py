00e483505b5fa644d932c94e8973e14d6dc019ce
NestorMonroy <nestor.monroy.90@gmail.com>
2020-05-22 12:53:57 -0500

    test
    v update

--- nblog/puppies/test.py
+++ nblog/puppies/test.py
@@ -3,8 +3,8 @@ from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
 
-# from ..models import Puppy
-from api.serializers import PuppySerializer
+from .models import Puppy
+from .api.serializers import PuppySerializer
 
 client = Client()
 
