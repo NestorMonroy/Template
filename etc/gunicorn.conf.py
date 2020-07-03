9fc9b7488f1cba81a5469cc7349a2bcc5ee60dd6
NestorMonroy <nestor.monroy.90@gmail.com>
2020-07-03 01:41:16 -0500

    update configuration server

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -3,4 +3,4 @@ bind = '127.0.0.1:8000'
 workers = 1
 timeout = 60
 errorlog = '/usr/local/apps/blog-nestor/nblog.gunicorng.error'
-accesslog = '/usr/local/apps/blog-nestor/nblog.gunicorng.error'
+accesslog = '/usr/local/apps/blog-nestor/nblog.gunicorng.access'
