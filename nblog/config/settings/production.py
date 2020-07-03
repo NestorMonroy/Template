f8acc7893ee652346c8caf14b07d513a42d99c10
NestorMonroy <nestor.monroy.90@gmail.com>
2020-07-03 02:32:04 -0500

    update configuration server

--- nblog/config/settings/production.py
+++ nblog/config/settings/production.py
@@ -2,18 +2,3 @@ from .base import *
 
 PRODUCTION = True
 
-
-ALLOWED_HOSTS = ['www.nestormonroy.com']
-
-
-if PRODUCTION:
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    CORS_REPLACE_HTTPS_REFERER      = True
-    HOST_SCHEME                     = "https://"
-    SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-    SECURE_HSTS_SECONDS             = 1000000
-    SECURE_FRAME_DENY               = True
-# Let's Encrypt ssl/tls https
