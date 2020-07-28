f44fe6e7cc432ac30624a9696427adc6755f2aa0
NestorMonroy <nestor.monroy.90@gmail.com>
2020-07-28 18:34:36 -0500

    adding grommet

---
+++ nblog/frontend/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>Nestor Blog</Heading>
+          <Button 
+            icon={<Notification />} 
+            onClick={() => { }} />
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app Body
+          </Box>
+          <Box
+             width='medium'
+             background='light-2'
+             elevation='small'
+             align='center'
+             justify='center'
+            >
+            sidebar
+          </Box>
+
+        </Box>
+
+
+      </Box>
+    </Grommet>
+  );
+}
+
+export default App;
