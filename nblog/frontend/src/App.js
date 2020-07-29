fe39591fb9bc0c4062d1e82f633f7d580f1c972a
NestorMonroy <nestor.monroy.90@gmail.com>
2020-07-28 22:28:12 -0500

    Collapsible

--- nblog/frontend/src/App.js
+++ nblog/frontend/src/App.js
@@ -1,16 +1,68 @@
 import React, { useState } from "react";
-import { Box, Button, Collapsible, Heading, Grommet, ResponsiveContext } from 'grommet';
-import { Notification } from 'grommet-icons';
+import { Box, Button, Collapsible, Heading, Grid, Grommet, Layer, ResponsiveContext } from 'grommet';
+import { Notification, FormClose } from 'grommet-icons';
 
 const theme = {
   global: {
     colors: {
-      brand: '#228BE6',
+      brand: '#ee9933',
+      'brand-contrast': '#ee993333',
+      active: '#eecc33',
     },
     font: {
       family: 'Roboto',
       size: '18px',
       height: '20px',
+    },
+  },
+  button: {
+    default: {
+      color: 'text',
+      border: undefined,
+      padding: {
+        horizontal: '12px',
+        vertical: '8px',
+      },
+    },
+    primary: {
+      background: { color: 'brand' },
+      border: undefined,
+      color: 'text-strong',
+      font: { weight: 'bold' },
+      padding: {
+        horizontal: '12px',
+        vertical: '8px',
+      },
+    },
+    secondary: {
+      border: { color: 'brand', width: '4px' },
+      color: 'text',
+      padding: {
+        horizontal: '8px',
+        vertical: '4px',
+      },
+    },
+    active: {
+      background: { color: 'brand-contrast' },
+      color: 'text',
+      secondary: {
+        background: 'none',
+        border: {
+          color: 'brand-contrast',
+        },
+      },
+    },
+    disabled: {
+      opacity: 0.3,
+      secondary: {
+        border: { color: 'text-weak' },
+      },
+    },
+    hover: {
+      background: { color: 'active' },
+      secondary: {
+        border: { color: 'active' },
+      },
     },
   },
 };
@@ -33,32 +85,61 @@ function App() {
   const [showSidebar, setShowSidebar] = useState(false);
   return (
     <Grommet theme={theme} full>
-      <Box fill>
-        <AppBar>
-          <Heading level='3' margin='none'>Nestor Blog</Heading>
-          <Button 
-            icon={<Notification />} 
-            onClick={() => setShowSidebar(!showSidebar)}
-            />
-        </AppBar>
-        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-          <Box flex align='center' justify='center'>
-            app Body
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>Nestor Blog</Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align='center' justify='center'>
+                app Body
           </Box>
-          <Collapsible direction="horizontal" open={showSidebar}>
-            <Box
-               width='medium'
-               background='light-2'
-               elevation='small'
-               align='center'
-               justify='center'
-              >
-              sidebar
+              {(!showSidebar || size !== 'small') ? (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    width='medium'
+                    background='light-2'
+                    elevation='small'
+                    align='center'
+                    justify='center'
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              ) : (
+                  <Layer>
+                    <Box
+                      background='light-2'
+                      tag='header'
+                      justify='end'
+                      align='center'
+                      direction='row'
+                    >
+                      <Button
+                        icon={<FormClose />}
+                        onClick={() => setShowSidebar(false)}
+                      />
+                    </Box>
+                    <Box
+                      fill
+                      background='light-2'
+                      align='center'
+                      justify='center'
+                    >
+                      sidebar
+                      </Box>
+                  </Layer>
+                )}
             </Box>
-            </Collapsible>
-        </Box>
-      </Box>
-    </Grommet>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet >
   );
 }
 
