c8d97d7697d8bcb9b86b551917d494e7b66ad067
NestorMonroy <nestor.monroy.90@gmail.com>
2020-08-12 23:44:12 -0500

    update frontend

--- nblog/frontend/src/views/Home/Home.js
+++ nblog/frontend/src/views/Home/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+
 import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
+import { Button, Grid, Hidden, Typography } from "@material-ui/core";
 import { Page } from "../../components";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import PropTypes from "prop-types";
 
 import {
   Header,
   TodaysMoney,
+  
   // FAQ,
   // PluginsSupport,
   // SourceFiles,
@@ -13,6 +18,7 @@ import {
 } from './components';
 
 import Post from 'src/views/Post'
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,34 +26,76 @@ const useStyles = makeStyles(theme => ({
   },
   container: {
     marginTop: theme.spacing(3)
-  }
+  },
+  summaryButton: {
+    backgroundColor: theme.palette.white,
+  },
+  barChartIcon: {
+    marginRight: theme.spacing(1),
+  },
+  image: {
+    width: "100%",
+    maxHeight: 400,
+  },
 }));
 
 
-const Home = () => {
+const Home = (props) => {
+  const { className, ...rest } = props;
+  const history = useHistory();
   const classes = useStyles();
+  const [user, setUser] = useState({
+    sub: "nestor.monroy.90@gmail.com",
+    scopes: "ROLE_ADMIN,ROLE_USER",
+    first_name: "Nestor",
+    last_name: "Monroy",
+      
+  });
+
+  const handleClick = () => {
+    history.push("/post");
+  };
 
   return (
-    <Page
-      className={classes.root}
-      title="Presentación"
-    >
-      <Post />
-      {/* <Header /> */}
-      {/* <Grid
-        className={classes.container}
-        container
-        spacing={3}
-      >
-        <Grid
-          item
-          xs={12}
-        > 
-          <PostsList />
+    <div {...rest} className={clsx(classes.root, className)}>
+      <Grid alignItems="center" container justify="space-between" spacing={3}>
+        <Grid item md={6} xs={12}>
+          <Typography component="h2" gutterBottom variant="overline">
+            Home
+          </Typography>
+          <Typography component="h1" gutterBottom variant="h3">
+            Hi, {user.first_name.toUpperCase()}{" "}
+            {user.last_name.toUpperCase()}
+          </Typography>
+          <Typography gutterBottom variant="subtitle1">
+            Here’s what’s happening with your projects today
+          </Typography>
+          <Button
+            className={classes.summaryButton}
+            edge="start"
+            variant="contained"
+            onClick={handleClick}
+
+          >
+            <BarChartIcon className={classes.barChartIcon} />
+            View Post
+          </Button>
         </Grid>
-      </Grid> */}
-    </Page>
+        <Hidden smDown>
+          <Grid item md={6}>
+            <img
+              alt="Cover"
+              className={classes.image}
+              src="/images/undraw_growth_analytics_8btt.svg"
+            />
+          </Grid>
+        </Hidden>
+      </Grid>
+    </div>
   );
 };
 
+Home.propTypes = {
+  className: PropTypes.string,
+};
 export default Home;
